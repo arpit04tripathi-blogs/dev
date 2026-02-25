4a1bc3c0a60ffcb25eb3fa8df9cdae79f593885f
Arpit Tripathi <arpit04tripathi@gmail.com>
2026-02-25 19:58:12 +0000

    component: NotFound

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,6 +2,7 @@
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
+import { NotFound } from '../../src/components/index.ts';
 import './style.css'
 
 export default {
@@ -9,6 +10,7 @@ export default {
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
+      'not-found': () => h(NotFound)
     })
   },
   enhanceApp({ app, router, siteData }) {
