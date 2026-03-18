4156980e2e66adcb7f137820490f7f6680b585ae
Arpit Tripathi <arpit04tripathi@gmail.com>
2026-03-18 16:18:06 +0000

    empty pages : cse

--- docs/.vitepress/theme/navs/sidebars/cse/default.ts
+++ docs/.vitepress/theme/navs/sidebars/cse/default.ts
@@ -1,13 +1,14 @@
 import { default as PATHS } from '../../paths';
 
 const SIDEBAR_DEFAULT_CSE = [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: PATHS.MISC_MARKDOWN_EXAMPLES },
-          { text: 'Runtime API Examples', link: PATHS.MISC_API_EXAMPLES },
-        ]
-      },
+      { text: 'Data Structures', link: PATHS.CSE_DS },
+      { text: 'Algorithms', link: PATHS.CSE_ALGO },
+      { text: 'Operating Systems (OS)', link: PATHS.CSE_OS },
+      { text: 'DBMS', link: PATHS.CSE_DBMS },
+      { text: 'Computer Networks', link: PATHS.CSE_CN },
+      { text: 'Programming', link: PATHS.CSE_PROG_BASIC },
+      { text: 'Cheat Sheet', link: PATHS.CSE_CHEATSHEET },
+      { text: 'Puzzles', link: PATHS.CSE_PUZZLES },
     ]
 
 export default SIDEBAR_DEFAULT_CSE;
