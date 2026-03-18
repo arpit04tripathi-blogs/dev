1835a9ddb415874b621bf3707311c32cfce65a89
Arpit Tripathi <arpit04tripathi@gmail.com>
2026-03-18 16:09:23 +0000

    empty pages : devops

--- docs/.vitepress/theme/navs/sidebars/devops/default.ts
+++ docs/.vitepress/theme/navs/sidebars/devops/default.ts
@@ -2,10 +2,34 @@ import { default as PATHS } from '../../paths';
 
 const SIDEBAR_DEFAULT_DEVOPS = [
       {
-        text: 'Examples',
+        // text: 'Version Control',
         items: [
-          { text: 'Markdown Examples', link: PATHS.MISC_MARKDOWN_EXAMPLES },
-          { text: 'Runtime API Examples', link: PATHS.MISC_API_EXAMPLES },
+          { text: 'Git', link: PATHS.DEVOPS_GIT },
+          { text: 'Gradle', link: PATHS.DEVOPS_GRADLE },
+        ]
+      },
+      {
+        // text: 'Tools',
+        items: [
+          { text: 'Jenkins', link: PATHS.DEVOPS_JENKINS },
+          { text: 'Puppet', link: PATHS.DEVOPS_PUPPET },
+          { text: 'Nginx', link: PATHS.DEVOPS_NGINX },
+        ]
+      },
+      {
+        text: 'Container Orchestration',
+        items: [
+          { text: 'Containerization', link: PATHS.DEVOPS_CONTAINERIZATION },
+          { text: 'Docker', link: PATHS.DEVOPS_DOCKER },
+          { text: 'Kubernetes', link: PATHS.DEVOPS_KUBERNETES },
+          { text: 'Hasicorp Nomad', link: PATHS.DEVOPS_NOMAD },
+        ]
+      },
+      {
+        text: 'Monitoring & Observability',
+        items: [
+          { text: 'ELK', link: PATHS.DEVOPS_ELK },
+          { text: 'Grafana', link: PATHS.DEVOPS_GRAFANA },
         ]
       },
     ]
