37ea0ceb84d36ad2c98e3c62f6128d49872b60eb
Arpit Tripathi <arpit04tripathi@gmail.com>
2026-03-18 15:30:27 +0000

    empty pages : swe

--- docs/.vitepress/theme/navs/sidebars/swe/default.ts
+++ docs/.vitepress/theme/navs/sidebars/swe/default.ts
@@ -2,10 +2,30 @@ import { default as PATHS } from '../../paths';
 
 const SIDEBAR_DEFAULT_SWE = [
       {
-        text: 'Examples',
+        text: 'Best Practices',
         items: [
-          { text: 'Markdown Examples', link: PATHS.MISC_MARKDOWN_EXAMPLES },
-          { text: 'Runtime API Examples', link: PATHS.MISC_API_EXAMPLES },
+          { text: 'Clean Code', link: PATHS.SWE_CLEAN_CODE },
+          { text: 'Design Patterns', link: PATHS.SWE_DESIGN_PATTERNS },
+        ]
+      },
+      {
+        text: 'SWE Concepts',
+        items: [
+          { text: 'Cloud Computing', link: PATHS.SWE_CLOUD_COMPUTING },
+          { text: 'Distributed Systems', link: PATHS.SWE_DISTRIBUTED_SYSTEMS },
+        ]
+      },
+      {
+        text: 'System Design',
+        items: [
+          { text: 'System Design Concepts', link: PATHS.SWE_SYSTEM_DESIGN },
+          { text: 'System Design Examples', link: PATHS.SWE_SYSTEM_DESIGN_EXAMPLES },
+        ]
+      },
+      {
+        text: 'Cloud Platform',
+        items: [
+          { text: 'Amazon Web Services (AWS)', link: PATHS.SWE_AWS },
         ]
       },
     ]
