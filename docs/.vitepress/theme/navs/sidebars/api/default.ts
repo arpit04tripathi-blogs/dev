01f91b8bacf44d9d0783adf36b31aa16654a0dfc
Arpit Tripathi <arpit04tripathi@gmail.com>
2026-03-18 16:47:23 +0000

    empty pages : api

--- docs/.vitepress/theme/navs/sidebars/api/default.ts
+++ docs/.vitepress/theme/navs/sidebars/api/default.ts
@@ -2,10 +2,33 @@ import { default as PATHS } from '../../paths';
 
 const SIDEBAR_DEFAULT_API = [
       {
-        text: 'Examples',
+        text: 'Core',
         items: [
-          { text: 'Markdown Examples', link: PATHS.MISC_MARKDOWN_EXAMPLES },
-          { text: 'Runtime API Examples', link: PATHS.MISC_API_EXAMPLES },
+          { text: 'Http', link: PATHS.API_HTTP },
+          { text: 'CORS', link: PATHS.API_CORS },
+          { text: 'REST', link: PATHS.API_REST },
+          { text: 'OWASP', link: PATHS.API_OWASP },
+        ]
+      },
+      {
+        text: 'Services',
+        items: [
+          { text: 'WebServices', link: PATHS.API_WEBSERVICES },
+          { text: 'Microservices', link: PATHS.API_MICROSERVICES },
+        ]
+      },
+      {
+        text: 'API Docs',
+        items: [
+          { text: 'Swagger', link: PATHS.API_SWAGGER },
+          { text: 'Open API', link: PATHS.API_OPENAPI },
+        ]
+      },
+      {
+        text: 'Messaging',
+        items: [
+          { text: 'Apache Kafka', link: PATHS.API_KAFKA },
+          { text: 'Rabbit MQ', link: PATHS.API_RABBIT_MQ },
         ]
       },
     ]
