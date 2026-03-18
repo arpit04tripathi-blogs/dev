8c2df0f73160f7bf5daf01460f420944b2d35821
Arpit Tripathi <arpit04tripathi@gmail.com>
2026-03-18 12:33:00 +0000

    move items

---
+++ docs/.vitepress/theme/navs/sidebars/backend/spring.ts
@@ -0,0 +1,23 @@
+const SPRING_SIDEBAR = [
+      { text: 'Spring Framework', link: '/spring/' },
+      {
+        text: 'Spring Core',
+        collapsed: true,
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' },
+          { text: 'Elements', link: '/elements' },
+        ]
+      },
+      {
+        text: 'Spring Web',
+        collapsed: true,
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' },
+          { text: 'Elements', link: '/elements' },
+        ]
+      }
+    ]
+
+export default SPRING_SIDEBAR;
